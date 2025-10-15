ae043095bcc98e0fe2fbcdc9d832f93b0adc80ed
rikbritt <richard.britt@live.co.uk>
2025-10-15 22:18:04 +0100

    cmake

--- file_list_gen.cmake
+++ file_list_gen.cmake
@@ -1,5 +1,7 @@
 list(
   APPEND
   PROJECT_SOURCES
+  ${CMAKE_CURRENT_LIST_DIR}/screens/screen_hello_world_gen.c
   ${CMAKE_CURRENT_LIST_DIR}/test_gen.c
   ${CMAKE_CURRENT_LIST_DIR}/test.c)
+  ${CMAKE_CURRENT_LIST_DIR}/fonts/font_medium_data.c)
